3b70ee95e83f0d666e731899f7df4d4ecd5d9999
SaRI-KRuPuK <funmii77@gmail.com>
2021-10-10 21:14:38 +0700

    Pong

---
+++ Pong/Assets/Script/Player1Score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player1Score : MonoBehaviour
+{
+
+    public PlayerControl player1; // skrip
+    public Text player1ScoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        player1ScoreText.text = player1.Score.ToString();
+    }
+}
---
+++ Pong/Assets/Script/Player2Score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player2Score : MonoBehaviour
+{
+
+    public PlayerControl player2; // skrip
+    public Text player2ScoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        player2ScoreText.text = player2.Score.ToString();
+    }
+}
